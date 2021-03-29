c38d705b426ed20a8559ef8d9047d84a2cb768b1
katerina <bromkate@mail.ru>
2021-03-29 23:08:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sharestracker/FirstFragment.java
@@ -0,0 +1,39 @@
+package com.example.sharestracker;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import org.w3c.dom.Text;
+
+public class FirstFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.favoritesText).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);*/
+                TextView text = getActivity().findViewById(R.id.favoritesText);
+                text.setLayoutParams(R.layout.favourite_in_focus);
+
+            }
+        });
+    }
+}
